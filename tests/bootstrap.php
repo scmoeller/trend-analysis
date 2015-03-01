03177be22f5744fc149c09c9edf9d4c98664bbac
stefan <stefan@stefan-laptop>
2015-03-01 17:41:03 +0100

    PHPUnit eingerichtet.

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author stefan
+ */
+// TODO: check include path
+ini_set(__DIR__, '/../', ini_get('include_path'));
+
+require_once '../vendor/autoload.php';
+?>
